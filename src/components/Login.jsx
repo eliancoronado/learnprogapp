efa6f84c6a725056acb8fee85333f8df2b34e243
eliancoronado <cuentaparaelian12@gmail.com>
2024-10-03 21:27:42 -0600

    first commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Loader from "./Loader"; // Asegúrate de tener un componente Loader creado o importado
@@ -10,11 +10,21 @@ const Login = () => {
     password: "",
   });
   const [loading, setLoading] = useState(false); // Estado para manejar el loader
+  const [isFilled, setIsFilled] = useState(false); // Estado para verificar si los inputs están llenos
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  useEffect(() => {
+    // Verificar si ambos campos están llenos
+    if (formData.email && formData.password) {
+      setIsFilled(true);
+    } else {
+      setIsFilled(false);
+    }
+  }, [formData]); // Ejecutar cada vez que formData cambie
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -44,32 +54,47 @@ const Login = () => {
       {loading && <Loader />}{" "}
       {/* Mostrar el loader mientras loading sea true */}
       <form onSubmit={handleSubmit}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="50%"
+          viewBox="0 0 24 24"
+          style={{ fill: "rgba(50, 189, 199, 1)", transform: "", msFilter: "" }} // Cambiar a objeto
+        >
+          <path d="M2 7v1l11 4 9-4V7L11 4z"></path>
+          <path d="M4 11v4.267c0 1.621 4.001 3.893 9 3.734 4-.126 6.586-1.972 7-3.467.024-.089.037-.178.037-.268V11L13 14l-5-1.667v3.213l-1-.364V12l-3-1z"></path>
+        </svg>
+
         <h1>LearnProg</h1>
         <h3>Inicia Sesión</h3>
+        <span></span>
         <div>
-          <label>Correo</label>
           <input
             type="email"
             name="email"
             onChange={handleChange}
             required
+            placeholder="Ingresa tu correo"
             disabled={loading} // Deshabilitar input cuando loading sea true
           />
         </div>
         <div>
-          <label>Contraseña</label>
           <input
             type="password"
             name="password"
             onChange={handleChange}
             required
+            placeholder="Ingresa tu contraseña"
             disabled={loading} // Deshabilitar input cuando loading sea true
           />
         </div>
-        <button type="submit" disabled={loading}>
+        <button
+          type="submit"
+          disabled={loading}
+          style={{ backgroundColor: isFilled ? "#38BDC7" : "#1E2426" }}
+        >
           {" "}
           {/* Deshabilitar botón si loading es true */}
-          {loading ? "Iniciando..." : "Login"}{" "}
+          {loading ? "Iniciando..." : "Iniciar Sesión"}{" "}
           {/* Texto alterno si está cargando */}
         </button>
         <p>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,6 +13,7 @@ const Register = () => {
     role: "student", // Valor por defecto
   });
   const [loading, setLoading] = useState(false); // Estado para manejar el loader
+  const [isFilled, setIsFilled] = useState(false); // Estado para verificar si los inputs están llenos
   const navigate = useNavigate();
 
   // Verificar si el usuario ya está autenticado
@@ -22,6 +23,15 @@ const Register = () => {
       navigate("/home"); // Redirigir si ya está autenticado
     }
   }, [navigate]);
+
+  useEffect(() => {
+    // Verificar si ambos campos están llenos
+    if (formData.username && formData.email && formData.password) {
+      setIsFilled(true);
+    } else {
+      setIsFilled(false);
+    }
+  }, [formData]); // Ejecutar cada vez que formData cambie
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -77,30 +87,51 @@ const Register = () => {
       {loading && <Loader />}{" "}
       {/* Mostrar el loader mientras loading sea true */}
       <form onSubmit={handleSubmit}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="50%"
+          viewBox="0 0 24 24"
+          style={{ fill: "rgba(50, 189, 199, 1)", transform: "", msFilter: "" }} // Cambiar a objeto
+        >
+          <path d="M2 7v1l11 4 9-4V7L11 4z"></path>
+          <path d="M4 11v4.267c0 1.621 4.001 3.893 9 3.734 4-.126 6.586-1.972 7-3.467.024-.089.037-.178.037-.268V11L13 14l-5-1.667v3.213l-1-.364V12l-3-1z"></path>
+        </svg>
         <h1>LearnProg</h1>
         <h3>Crea tu cuenta</h3>
+        <span></span>
         <div>
-          <label>Nombre</label>
-          <input type="text" name="username" onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Correo</label>
-          <input type="email" name="email" onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Contraseña</label>
           <input
-            type="password"
-            name="password"
+            type="text"
+            name="username"
             onChange={handleChange}
+            placeholder="Nombre de usuario"
             required
           />
         </div>
         <div>
-          <label>Imagen de perfil</label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            placeholder="Ingresa el correo"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            placeholder="Ingresa la contraseña"
+            required
+          />
+        </div>
+        <div>
+          <label>Imagen de perfil (Opcional)</label>
           <input type="file" name="profileImage" onChange={handleFileChange} />
         </div>
         <div>
+          {/* Deshabilitar botón si loading es true 
           <div className="rolsle">
             <label>Rol</label>
             <select
@@ -113,8 +144,13 @@ const Register = () => {
               <option value="teacher">Profesor</option>
             </select>
           </div>
+          */}
         </div>
-        <button type="submit" disabled={loading}>
+        <button
+          type="submit"
+          disabled={loading}
+          style={{ backgroundColor: isFilled ? "#38BDC7" : "#1E2426" }}
+        >
           {" "}
           {/* Deshabilitar botón si está cargando */}
           {loading ? "Registrando..." : "Registrar"}{" "}
